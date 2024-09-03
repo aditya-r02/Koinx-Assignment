54f0e8d4b17c78a592f0efec21144ceba09b547d
aditya-r02 <ranaaditya01@outlook.com>
2024-09-03 15:38:16 +0530

    Added home page html

--- index.js
+++ index.js
@@ -18,7 +18,21 @@ connect();
 app.use(express.json());
 
 app.get("/", (req, res)=> {
-    return res.send("Hello World!")
+    return res.send(`<div>
+            <h4>
+                Fetch Transactions:
+            </h4>
+            <a href="https://coinx-assignment-i17f.onrender.com/api/v1/fetchtransactions?address=0xce94e5621a5f7068253c42558c147480f38b5e0d">
+                https://coinx-assignment-i17f.onrender.com/api/v1/fetchtransactions?address=0xce94e5621a5f7068253c42558c147480f38b5e0d
+            </a>
+
+            <h4>
+                Fetch Expenses
+            </h4>
+            <a href="https://coinx-assignment-i17f.onrender.com/api/v1/fetchexpenses?address=0xce94e5621a5f7068253c42558c147480f38b5e0d">
+                https://coinx-assignment-i17f.onrender.com/api/v1/fetchexpenses?address=0xce94e5621a5f7068253c42558c147480f38b5e0d
+            </a>
+        </div>`)
 })
 
 // Server starting on port 3000
